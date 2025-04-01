91a0040a33decd62ec11534a207a7929dc46adb4
Aaditya Karamchandani <unredaaditya36@gmail.com>
2025-04-01 10:50:47 -0400

    Setting up CreateUser #1

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from "expo-status-bar";
+import * as React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+const Stack = createNativeStackNavigator();
+
+//The Main Starter Function
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Text>Welcome To WaveLength: </Text>
+        <Text>Sign UP or Sign In</Text>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
